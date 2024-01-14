c1f6ad0f1d7ff8daaf2bd11f25286fd2dea3d7da
Julián Arce <juarce@itba.edu.ar>
2024-01-14 12:44:11 -0300

    feat: implement recommended-for-id param to GET /prayer-requests

--- models/src/main/java/ar/juarce/models/PrayerRequestFilter.java
+++ models/src/main/java/ar/juarce/models/PrayerRequestFilter.java
@@ -1,17 +1,19 @@
 package ar.juarce.models;
 
-import jakarta.persistence.criteria.CriteriaBuilder;
-import jakarta.persistence.criteria.CriteriaQuery;
-import jakarta.persistence.criteria.Root;
+import jakarta.persistence.criteria.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class PrayerRequestFilter implements Filter<PrayerRequest> {
 
     private final Long requesterId;
+    private final Long recommendedForId;
 
-    public PrayerRequestFilter(Long requesterId) {
+    public PrayerRequestFilter(Long requesterId, Long recommendedForId) {
         this.requesterId = requesterId;
+        this.recommendedForId = recommendedForId;
     }
 
     @Override
@@ -21,8 +23,25 @@ public class PrayerRequestFilter implements Filter<PrayerRequest> {
 
     @Override
     public void setFiltersToQuery(CriteriaBuilder criteriaBuilder, CriteriaQuery<PrayerRequest> criteriaQuery, Root<PrayerRequest> root) {
+        List<Predicate> predicates = new ArrayList<>();
         if (requesterId != null) {
-            criteriaQuery.where(criteriaBuilder.equal(root.get("requester").get("id"), requesterId));
+            predicates.add(criteriaBuilder
+                    .equal(root.get("requester").get("id"), requesterId));
+        }
+        if (recommendedForId != null) {
+            predicates.add(criteriaBuilder
+                    .notEqual(root.get("requester").get("id"), recommendedForId));
+
+            Subquery<Long> subquery = criteriaQuery.subquery(Long.class);
+            Root<Prayer> prayerRoot = subquery.from(Prayer.class);
+            subquery.select(prayerRoot.get("prayerRequest").get("id"))
+                    .distinct(true)
+                    .where(criteriaBuilder.equal(prayerRoot.get("believer").get("id"), recommendedForId));
+            predicates.add(criteriaBuilder
+                    .not(root.get("id").in(subquery)));
+        }
+        if (!predicates.isEmpty()) {
+            criteriaQuery.where(predicates.toArray(Predicate[]::new));
         }
     }
 
@@ -32,14 +51,20 @@ public class PrayerRequestFilter implements Filter<PrayerRequest> {
 
     public static class Builder {
         private Long requesterId;
+        private Long recommendedForId;
 
         public Builder requesterId(Long requesterId) {
             this.requesterId = requesterId;
             return this;
         }
 
+        public Builder recommendedForId(Long recommendedForId) {
+            this.recommendedForId = recommendedForId;
+            return this;
+        }
+
         public PrayerRequestFilter build() {
-            return new PrayerRequestFilter(requesterId);
+            return new PrayerRequestFilter(requesterId, recommendedForId);
         }
     }
 }
