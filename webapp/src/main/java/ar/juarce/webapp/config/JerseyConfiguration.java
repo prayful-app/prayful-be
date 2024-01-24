2eff26673916ad74f6d22e5ed9629aadaa5d319a
Julián Arce <juarce@itba.edu.ar>
2024-01-23 22:53:04 -0300

    build: don't use packages for endpoints and register them manually instead

--- webapp/src/main/java/ar/juarce/webapp/config/JerseyConfiguration.java
+++ webapp/src/main/java/ar/juarce/webapp/config/JerseyConfiguration.java
@@ -1,5 +1,10 @@
 package ar.juarce.webapp.config;
 
+import ar.juarce.webapp.controllers.HomeController;
+import ar.juarce.webapp.controllers.PrayerController;
+import ar.juarce.webapp.controllers.PrayerRequestController;
+import ar.juarce.webapp.controllers.UserController;
+import ar.juarce.webapp.exceptionMappers.*;
 import jakarta.annotation.PostConstruct;
 import jakarta.ws.rs.ApplicationPath;
 import org.glassfish.jersey.server.ResourceConfig;
@@ -11,8 +16,23 @@ public class JerseyConfiguration extends ResourceConfig {
 
     @PostConstruct
     public void init() {
-        packages("ar.juarce.webapp.controllers");
-        packages("ar.juarce.webapp.exceptionMappers");
+        /*
+        https://github.com/spring-projects/spring-boot/issues/7496#issuecomment-263245202
+        https://github.com/spring-projects/spring-boot/issues/7496
+        https://github.com/spring-projects/spring-boot/blob/e15b3e463f312524495349673a16cb67cfaa2eae/spring-boot-samples/spring-boot-sample-jersey/src/main/java/sample/jersey/JerseyConfig.java#L27-L28
+        Don't use packages to scan for endpoints and register them individually instead.
+         */
+        register(HomeController.class);
+        register(PrayerController.class);
+        register(PrayerRequestController.class);
+        register(UserController.class);
+
+        register(AlreadyExistsExceptionMapper.class);
+        register(ClientErrorExceptionMapper.class);
+        register(ConstraintViolationExceptionMapper.class);
+        register(GenericExceptionMapper.class);
+        register(NotFoundExceptionMapper.class);
+
         register(ObjectMapperProvider.class);
     }
 }
